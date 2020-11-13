4ee761e63acdfedc6887a5170faffffefafcf0a6
Anson <1072476988@qq.com>
2020-11-13 14:11:31 +0800

    finish async.mock.

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,4 +1,5 @@
 import { register } from "../user";
+import { verifyUsername } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
@@ -6,9 +7,12 @@ jest.mock("axios");
 describe("register", () => {
   test("should post user when validated", () => {
     // TODO 19: add test here
+    register("123","123").then(data => expect(data).toEqual({}))
   });
 
   test("should reject with Error when username is invalid", () => {
     // TODO 20: add test here
+    verifyUsername.mockImplementation(() => false)
+    expect(register("123","123")).rejects.toEqual(new Error("wrong username or password"));
   });
 });
